e2a3951a156b24f8244d75711aeff7abc0cd86d8
ChiChiBeek <me@thijs.com>
2026-01-30 11:35:19 +0100

    Update winner to HendrikWins

--- src/App.jsx
+++ src/App.jsx
@@ -359,7 +359,7 @@ useEffect(() => {
         <p style={{ fontSize: "1.5rem", marginTop: 6 }}>{renderHearts(winner.lives)}</p>
 
         <video
-          src="/lucaswinner.mp4"
+          src="/HendrikWins.mp4"
           autoPlay
           loop={false}
           muted={false}
